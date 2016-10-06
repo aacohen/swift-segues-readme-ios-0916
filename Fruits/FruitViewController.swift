f3a0e05b98b1e9a95f81e34f21ffac3961d371ef
aacohen <arielaades@gmail.com>
2016-10-06 14:56:04 -0400

    completed read me

--- Fruits/FruitViewController.swift
+++ Fruits/FruitViewController.swift
@@ -14,6 +14,7 @@ class FruitViewController: UIViewController {
 
     var fruit: String?
 
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if let fruit = fruit {
